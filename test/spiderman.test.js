ec180faf031d59ff1ee7fbbd00bc3dbfc14933b4
Drako9159 <chochito9159@gmail.com>
2022-04-19 13:32:17 -0500

    actualizando contenido de spiderverse

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,21 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('../app/spiderman.js')
+/*describe("Unit test for spiderman class", () => {
+    test('1)Create an spiderman objet', () => {
+        //Código a usar
+        //Instanciar un objeto Spiderman con parámetros
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 31, "Sony")
+        //Validamos con el código
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
+  })*/
+  describe("Unit test for spiderman class", () => {
+      test('1)Create an spiderman objet',() => {
+          const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+          expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+
+      })
   })
